390b52a194c786133f35a6e6add0252aacaa29cd
yuuiwqy622 <random064@gmail.com>
2020-10-29 12:35:21 +0100

    select and empty triangle

--- tr.js
+++ tr.js
@@ -1,3 +1,31 @@
+function selectTr(e) {
+    e.preventDefault()
+    let l = e.target.classList
+    if (l.contains('selected')) {
+        l.remove('selected')
+        return
+    }
+
+    l.add('selected')
+}
+
+function emptyTr(e) {
+    if (e.which != 1) return
+
+    let l = e.target.classList
+    if (l.contains('empty')) {
+        l.remove('empty')
+        return
+    }
+
+    l.add('empty')
+}
+
 document.addEventListener('DOMContentLoaded', (e) => {
     let svg = document.getElementById('trSVG')
+    let trs = svg.getElementsByTagName('polygon')
+
+    for (let t of trs) t.addEventListener('contextmenu', selectTr)
+    for (let t of trs) t.addEventListener('mousedown', emptyTr)
+
 })
