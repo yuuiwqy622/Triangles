aaa8d528027ef09a43cedf831a6abad2cd206ee6
yuuiwqy622 <random064@gmail.com>
2020-10-29 20:53:17 +0100

    Add files via upload

--- tr2.js
+++ tr2.js
@@ -15,23 +15,78 @@ function setVal(e) {
     console.log(e.target)
 }
 
+function createTable(lines) {
+    let n = parseInt(lines[0])
+    let m = 1 + 2 * (n - 1)
+    let t = document.querySelector('table')
+
+    for (let i = 0; i < n; ++i) {
+        let tr = document.createElement('tr')
+        for (let j = 0; j < m; ++j) {
+            let td = document.createElement('td')
+            td.textContent = +(lines[i + 1][j] == '-')
+            td.addEventListener('contextmenu', selectCell)
+            td.addEventListener('wheel', setVal)
+            tr.append(td)
+        }
+
+        t.append(tr)
+    }
+}
+
 function readFile(e) {
     let f = e.target.files[0]
     if (!f) return
 
     let rd = new FileReader()
     rd.onload = (e) => {
-        let content = e.target.result
-        console.log(content)
+        let lines = e.target.result.split(/[\r\n]+/g)
+        createTable(lines)
     }
     rd.readAsText(f)
 }
 
+function maxArea() {
+    let rows = document.querySelector('table').children
+    let n = rows.length
+    let m = rows[0].children.length
+    let area = 0
+
+    /* downwards triangles */
+    for (let i = 1; i < n; ++i)
+        for (let j = i; j < m - i; ++j) {
+            let td = rows[i].children[j]
+            td.classList.add('selected')
+            let sub1 = rows[i - 1].children[j - 1]
+            let sub2 = rows[i - 1].children[j]
+            let sub3 = rows[i - 1].children[j + 1]
+            sub1.classList.add('sub')
+            sub2.classList.add('sub')
+            sub3.classList.add('sub')
+
+            let a = parseInt(sub1.textContent)
+            let b = parseInt(sub2.textContent)
+            let c = parseInt(sub3.textContent)
+            let s = parseInt(td.textContent)
+
+            if (s) {
+                s = Math.min(a, b, c) + 1
+                td.textContent = s
+            }
+
+            area = Math.max(area, s)
+            sub1.classList.remove('sub')
+            sub2.classList.remove('sub')
+            sub3.classList.remove('sub')
+            td.classList.remove('selected')
+        }
+
+    /* upwards triangles */
+
+    alert(`Max area is ${area * area}`)
+}
+
 document.addEventListener('DOMContentLoaded', (e) => {
-    let cells = document.querySelectorAll('td')
-    for (c of cells) {
-        c.addEventListener('contextmenu', selectCell)
-        c.addEventListener('wheel', setVal)
-    }
     document.getElementById('file-input').addEventListener('change', readFile)
+    document.getElementById('start').addEventListener('click', maxArea)
 })
