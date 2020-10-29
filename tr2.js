4ee0f414c9832b3d4ccd819d80298e15aabd9bdd
yuuiwqy622 <random064@gmail.com>
2020-10-29 21:39:50 +0100

    Find area for upwards triangles too

--- tr2.js
+++ tr2.js
@@ -57,31 +57,68 @@ function maxArea() {
         for (let j = i; j < m - i; ++j) {
             let td = rows[i].children[j]
             td.classList.add('selected')
-            let sub1 = rows[i - 1].children[j - 1]
-            let sub2 = rows[i - 1].children[j]
-            let sub3 = rows[i - 1].children[j + 1]
-            sub1.classList.add('sub')
-            sub2.classList.add('sub')
-            sub3.classList.add('sub')
-
-            let a = parseInt(sub1.textContent)
-            let b = parseInt(sub2.textContent)
-            let c = parseInt(sub3.textContent)
             let s = parseInt(td.textContent)
 
             if (s) {
+                let sub1 = rows[i - 1].children[j - 1]
+                let sub2 = rows[i - 1].children[j]
+                let sub3 = rows[i - 1].children[j + 1]
+                sub1.classList.add('sub')
+                sub2.classList.add('sub')
+                sub3.classList.add('sub')
+
+                let a = parseInt(sub1.textContent)
+                let b = parseInt(sub2.textContent)
+                let c = parseInt(sub3.textContent)
                 s = Math.min(a, b, c) + 1
                 td.textContent = s
+
+                sub1.classList.remove('sub')
+                sub2.classList.remove('sub')
+                sub3.classList.remove('sub')
             }
 
             area = Math.max(area, s)
-            sub1.classList.remove('sub')
-            sub2.classList.remove('sub')
-            sub3.classList.remove('sub')
             td.classList.remove('selected')
         }
 
     /* upwards triangles */
+    for (let j = n - 2; j < m - n + 2; ++j) {
+        let td = rows[n - 2].children[j]
+        let v = parseInt(td.textContent)
+        v = v > 0 | 0
+        td.textContent = v
+    }
+
+    for (let i = n - 3; i >= 0; --i)
+        for (let j = i + 2; j < (n + n - i - 3); ++j) {
+            let td = rows[i].children[j]
+            td.classList.add('selected')
+            let s = parseInt(td.textContent)
+
+            if (s) {
+                let sub1 = rows[i + 1].children[j - 1]
+                let sub2 = rows[i + 1].children[j]
+                let sub3 = rows[i + 1].children[j + 1]
+                sub1.classList.add('sub')
+                sub2.classList.add('sub')
+                sub3.classList.add('sub')
+
+                let a = parseInt(sub1.textContent)
+                let b = parseInt(sub2.textContent)
+                let c = parseInt(sub3.textContent)
+                s = Math.min(a, b, c) + 1
+                td.textContent = s
+
+                sub1.classList.remove('sub')
+                sub2.classList.remove('sub')
+                sub3.classList.remove('sub')
+            }
+
+            area = Math.max(area, s)
+            td.classList.remove('selected')
+        }
+
 
     alert(`Max area is ${area * area}`)
 }
