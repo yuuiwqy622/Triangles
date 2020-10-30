4af90f9877f243a8bb3f5b1b3cadb4ae6bbffa8e
yuuiwqy622 <random064@gmail.com>
2020-10-30 12:11:26 +0100

    Reduced amount of computation

--- triangle.c
+++ triangle.c
@@ -19,11 +19,14 @@ void printArea(int n) {
   int m = (n - 1) * 2 + 1;
   int area = 0;
 
+  for (int j = 0; j < m; ++j)
+    area = max(area, tr[0][j]);
+
   /* downward triangles */
   for (int i = 1; i < n; ++i)
-    for (int j = i; j < m - i; ++j) {
-      if (tr[i][j]) {
-        tr[i][j] = min(min(tr[i - 1][j - 1], tr[i - 1][j]), tr[i - 1][j + 1]);
+    for (int j = i; j < m - i; j += 2) {
+      if (tr[i][j] && tr[i - 1][j]) {
+        tr[i][j] = min(tr[i - 1][j - 1], tr[i - 1][j + 1]);
         ++tr[i][j];
       }
 
@@ -31,21 +34,21 @@ void printArea(int n) {
     }
 
   /* upward triangles */
-  for (int j = n - 2; j < m - n + 2; ++j) {
-    tr[n - 2][j] = tr[n - 2][j] > 0;
+  for (int j = n - 3; n > 3 && j < m - n + 2; ++j) {
+    tr[n - 3][j] = tr[n - 3][j] > 0;
   }
 
-  for (int i = n - 3; i >= 0; --i)
-    for (int j = i + 2; j < (n + n - i - 3); ++j) {
-      if (tr[i][j]) {
-        tr[i][j] = min(min(tr[i + 1][j - 1], tr[i + 1][j]), tr[i + 1][j + 1]);
+  for (int i = n - 4; n > 3 && i >= 0; --i)
+    for (int j = i + 3; j < n + n - i - 3; j += 2) {
+      if (tr[i][j] && tr[i + 1][j]) {
+        tr[i][j] = min(tr[i + 1][j - 1], tr[i + 1][j + 1]);
         ++tr[i][j];
       }
 
       area = max(area, tr[i][j]);
     }
 
-  printf("The largest triangle area is %d.\n", area * area);
+  printf("The largest triangle area is %d.\n\n", area * area);
 }
 
 int main(void) {
